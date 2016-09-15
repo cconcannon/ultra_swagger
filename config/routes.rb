3a1ac7eac5417738aba8475b9bfd77cb49c05e54
Chris Concannon <concach00@gmail.com>
2016-09-15 15:01:07 -0600

    heroku strava login troubleshooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
   
-  resources :users, only: :show
+  resources :users, only: [:show]
   
   get "/auth/strava/callback", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
