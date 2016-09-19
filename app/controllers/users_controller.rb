bb061c33673f6c3698c47d412d99378822e4b145
Chris Concannon <concach00@gmail.com>
2016-09-19 16:03:24 -0600

    products show reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = current_user
     @approved_reviews = @user.reviews.where(approved: true)
     @pending_reviews = @user.reviews.where(reviewed: false)
-    @rejected_reviews = @user.reviews.where(reviewed: false, approved: false)
+    @rejected_reviews = @user.reviews.where("reviewed = ? AND approved = ?", true, false)
     @strava_data = @user.strava_user_totals.last
   end
 end
