c5c94333b554eb86ad105810354e59e2edc9ca04
Chris Concannon <concach00@gmail.com>
2016-09-21 17:52:56 -0600

    feature spec passing

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,9 +1,10 @@
 class RacesController < ApplicationController
   def index
-    @races = Race.all
+    @races = Race.all.by_name_date
   end
   
   def show
     @race = Race.find(params[:id])
+    @reviews = Review.where(race: @race).all.by_item
   end
 end
